c4a5502489f215a4f0ffb003f5b5b0eb4e498dbf
Lovepreet singh <lovepreet.singh@antiersolutions.com>
2024-01-02 18:51:24 +0530

    animation

--- react/src/components/pages/Scroll/Scroll.js
+++ react/src/components/pages/Scroll/Scroll.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Animation from './Animation/Animation'
 import Animation2 from './Animation2/Animation2'
+import Animation3 from './Animation3/Animation3'
 
 const Scroll = () => {
     return (
@@ -8,6 +9,7 @@ const Scroll = () => {
             <Animation />
             <div style={{ height: 100 }} />
             <Animation2 />
+            <Animation3 />
         </>
     )
 }
